0d80bc874412d3bc78ddff15b77323c931d8f6fe
Florian Becker <1367506+make-ing@users.noreply.github.com>
2020-10-24 23:01:56 +0200

    add namespace so it will render in the browser

--- api/index.py
+++ api/index.py
@@ -6,6 +6,6 @@ class handler(BaseHTTPRequestHandler):
         self.send_response(200)
         self.send_header('Content-type','image/svg+xml')
         self.end_headers()
-        message = f'<svg><text x="5" y="5">Hello {self.headers["x-real-ip"]}! You are using {self.headers["User-Agent"]}. Thank you very much for visiting my Github profile.</text></svg>'
+        message = f'<svg fill="none" xmlns="http://www.w3.org/2000/svg"><text x="5" y="5">Hello {self.headers["x-real-ip"]}! You are using {self.headers["User-Agent"]}. Thank you very much for visiting my Github profile.</text></svg>'
         self.wfile.write(message.encode())
         return
