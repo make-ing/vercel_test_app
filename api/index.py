2fd0af3bf31c9c5158260f68841f573be2821253
Florian Becker <1367506+make-ing@users.noreply.github.com>
2020-10-24 22:57:47 +0200

    Try to give an svg as response

--- api/index.py
+++ api/index.py
@@ -1,12 +1,11 @@
 from http.server import BaseHTTPRequestHandler
-from cowpy import cow
+
 
 class handler(BaseHTTPRequestHandler):
-
     def do_GET(self):
         self.send_response(200)
-        self.send_header('Content-type','text/plain')
+        self.send_header('Content-type','image/svg+xml')
         self.end_headers()
-        message = cow.Cowacter().milk(f'Hello {self.headers["x-real-ip"]}! You are using {self.headers["User-Agent"]}. Thank you very much for visiting my Github profile.')
+        message = f'<svg><text x="5" y="5">Hello {self.headers["x-real-ip"]}! You are using {self.headers["User-Agent"]}. Thank you very much for visiting my Github profile.</text></svg>'
         self.wfile.write(message.encode())
         return
