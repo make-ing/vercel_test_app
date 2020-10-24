f694ec63ad1fb48096acae4c83a94f887135a9c5
Florian Becker <1367506+make-ing@users.noreply.github.com>
2020-10-24 22:46:12 +0200

    Add more text and info

--- api/index.py
+++ api/index.py
@@ -7,6 +7,6 @@ class handler(BaseHTTPRequestHandler):
         self.send_response(200)
         self.send_header('Content-type','text/plain')
         self.end_headers()
-        message = cow.Cowacter().milk(f'Hello {self.headers["x-real-ip"]}')
+        message = cow.Cowacter().milk(f'Hello {self.headers["x-real-ip"]}! You are using {self.headers["User-Agent"]}. Thank you very much for visiting my Github profile.')
         self.wfile.write(message.encode())
         return
