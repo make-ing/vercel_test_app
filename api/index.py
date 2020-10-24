4ad2c4c118263c8dd1670e27f47f1914b3c44c82
Florian Becker <1367506+make-ing@users.noreply.github.com>
2020-10-24 23:04:49 +0200

    add styling

--- api/index.py
+++ api/index.py
@@ -6,6 +6,6 @@ class handler(BaseHTTPRequestHandler):
         self.send_response(200)
         self.send_header('Content-type','image/svg+xml')
         self.end_headers()
-        message = f'<svg fill="none" xmlns="http://www.w3.org/2000/svg"><text x="5" y="5">Hello {self.headers["x-real-ip"]}! You are using {self.headers["User-Agent"]}. Thank you very much for visiting my Github profile.</text></svg>'
+        message = f'<svg fill="none" xmlns="http://www.w3.org/2000/svg"><style>.header {font: 600 18px "Segoe UI", Ubuntu, Sans-Serif; fill: #fe428e; animation: fadeInAnimation 0.8s ease-in-out forwards;}</style><text class="header" x="5" y="5">Hello {self.headers["x-real-ip"]}! You are using {self.headers["User-Agent"]}. Thank you very much for visiting my Github profile.</text></svg>'
         self.wfile.write(message.encode())
         return
