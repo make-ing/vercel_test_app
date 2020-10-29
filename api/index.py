3894abc5b9687351c3c3e04b5859d125aa346be5
make-ing <bckr.florian@gmail.com>
2020-10-29 07:45:48 +0100

    simple textual change

--- api/index.py
+++ api/index.py
@@ -15,10 +15,9 @@ class handler(BaseHTTPRequestHandler):
             style='font: 600 18px "Segoe UI", Ubuntu, Sans-Serif; fill: #fe428e;'
         ))
         dwg.add(dwg.text(
-            f"Thanks for visiting my Profile Page",
+            f"Thanks for visiting my Profile Page!",
             insert=(5, 50),
             style='font: 600 18px "Segoe UI", Ubuntu, Sans-Serif; fill: #fe428e;'
         ))
-        #message = f'<svg width="850" height="100" fill="none" xmlns="http://www.w3.org/2000/svg"><style>.header {{font: 600 18px "Segoe UI", Ubuntu, Sans-Serif; fill: #fe428e; animation: fadeInAnimation 0.8s ease-in-out forwards;\}}</style><text class="header" x="5" y="20">Hello.. it is now {datetime.now(tz=timezone.utc)}</text><text class="header" x="5" y="50">Thank you very much for visiting my Github profile.</text></svg>'
         self.wfile.write(dwg.tostring().encode())
         return
